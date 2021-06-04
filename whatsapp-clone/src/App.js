3ee6b30c0672b3312ffb4025ba673669757b074e
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-04 23:03:51 +0000

    css: body

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="App__Body">
+        {/* sidebar */}
+        {/* chat */}
+      </div>
     </div>
   );
 }
