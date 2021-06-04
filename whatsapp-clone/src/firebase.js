633dcd24816d680e0f6410e29a0e83c279a2e1c4
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-04 22:44:40 +0000

    firebase: setup

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAVxU6rH53CNS88aHn9NeHuKqe0395qMjU",
+    authDomain: "clones-81b78.firebaseapp.com",
+    projectId: "clones-81b78",
+    storageBucket: "clones-81b78.appspot.com",
+    messagingSenderId: "528571263130",
+    appId: "1:528571263130:web:25bdb5ce13e6d15261d723",
+    measurementId: "G-BYBT6BX5PW"
+  };
