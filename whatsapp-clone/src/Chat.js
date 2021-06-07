0273abb365fcbf5027189b34c708adc71eb0d588
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-07 23:43:09 +0000

    fix(footer): clean text after message submit

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -17,6 +17,7 @@ function Chat(){
     const sendMessage = (e) => {
         e.preventDefault();
         console.log(`you typed ${input}`)
+        setInput("")
     }
 
     return <div className="chat">
