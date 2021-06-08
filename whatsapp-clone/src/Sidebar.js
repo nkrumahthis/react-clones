0f788f6481b80689dad3ea5ac21d3d7214e58dec
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-08 23:43:36 +0000

    one step

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import SidebarChat from './SidebarChat';
+
+import db from "./firebase";
 
 import './Sidebar.css'
 
@@ -12,6 +14,20 @@ import SearchOutlined from '@material-ui/icons/SearchOutlined'
 
 
 function Sidebar() {
+
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot =>{
+            setRooms(snapshot.docs.map(doc =>(
+                {
+                    id: doc.id,
+                    data: doc.data(),
+                }
+            )))
+        })
+    }, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -30,20 +46,9 @@ function Sidebar() {
             </div>
             <div className="sidebar__chats">
                 <SidebarChat addNewChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
+                {rooms.map((room) => {
+                    <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
+                })}
             </div>
         </div>
     )
--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/app";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAVxU6rH53CNS88aHn9NeHuKqe0395qMjU",
     authDomain: "clones-81b78.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
     appId: "1:528571263130:web:25bdb5ce13e6d15261d723",
     measurementId: "G-BYBT6BX5PW"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
