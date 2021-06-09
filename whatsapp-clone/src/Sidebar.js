b87a577b44182eef6debf0f765dd707c17288e52
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-09 23:47:17 +0000

    fix: rooms sidebar

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -46,9 +46,9 @@ function Sidebar() {
             </div>
             <div className="sidebar__chats">
                 <SidebarChat addNewChat/>
-                {rooms.map((room) => {
+                {rooms.map((room) => (
                     <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
-                })}
+                ))}
             </div>
         </div>
     )
