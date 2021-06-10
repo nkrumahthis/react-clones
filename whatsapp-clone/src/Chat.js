1c167307567d1b3214c27eff10565ae87db22a53
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-10 22:41:50 +0000

    feat: on room change set room name, change avatar

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import "./Chat.css"
+import db from "./firebase";
 
 import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined, AttachFile, MoreVert, InsertEmoticon, Mic } from '@material-ui/icons';
@@ -9,10 +11,20 @@ function Chat(){
 
     const [seed, setSeed] = useState("");
     const [input, setInput] = useState("");
+    const { roomId } = useParams();
+    const [ roomName, setRoomName ] = useState("");
+
+    useEffect(()=>{
+        if(roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot =>{
+                setRoomName(snapshot.data().name)
+            })
+        }
+    }, [roomId])
     
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
-    }, []);
+    }, [roomId]);
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -24,7 +36,7 @@ function Chat(){
         <div className="chat__header">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="chat__headerInfo">
-                <h3>Room name</h3>
+                <h3>{roomName}</h3>
                 <p>Last seen at ...</p>
             </div>
             <div className="chat__headerRight">
