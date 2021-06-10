654ba955e7caf1d8897699bb7cbb18cf35a7c2f4
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-10 22:41:16 +0000

    feat: change chat based on room

--- whatsapp-clone/src/SidebarChat.js
+++ whatsapp-clone/src/SidebarChat.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Avatar } from '@material-ui/core';
+
+
 
 import './SidebarChat.css';
 
@@ -25,14 +28,16 @@ function SidebarChat({addNewChat, id, name}) {
     }
 
     return !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-            <div className="sidebarChat__info">
-                <h2>{name}</h2>
-                <p>Last message ...</p>
-                
+        <Link to={`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>Last message ...</p>
+                    
+                </div>
             </div>
-        </div>
+        </Link>
     ) : (
         <div onClick={createChat} className="sidebarChat">
             <h2>Add new Chat</h2>
