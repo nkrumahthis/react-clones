3eeac576303863309294d76b0caca8fa8f1f8816
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-10 22:14:05 +0000

    feat: routes

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <div className="App__Body">
-        {/* sidebar */}
-        <Sidebar />
-        {/* chat */}
-        <Chat />
+        <BrowserRouter>
+          <Switch>
+          <Sidebar />
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Home Screen</h1>
+            </Route>
+          </Switch>
+          </BrowserRouter>
       </div>
     </div>
   );
