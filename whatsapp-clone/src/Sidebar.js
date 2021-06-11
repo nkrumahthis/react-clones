d1892e73110c8558742a9832b9de74ea26fd9536
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-11 12:58:02 +0000

    feat: sidebar user photo

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -10,12 +10,14 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import { useStateValue } from './StateProvider';
 
 
 
 function Sidebar() {
 
     const [rooms, setRooms] = useState([])
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
 
@@ -37,7 +39,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
                 <div className="sidebar__headerRight">
                     <IconButton><DonutLargeIcon /></IconButton>
                     <IconButton><ChatIcon/></IconButton>
