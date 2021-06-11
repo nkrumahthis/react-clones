05619a9e05fcc193adc3b71b92def31f4a90b99a
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-11 17:09:07 +0000

    feat: last seen

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -51,7 +51,10 @@ function Chat(){
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="chat__headerInfo">
                 <h3>{roomName}</h3>
-                <p>Last seen at ...</p>
+                <p>last seen {" "} 
+                {new Date(
+                    messages[messages.length - 1]?.timestamp?.toDate()
+                ).toUTCString()}</p>
             </div>
             <div className="chat__headerRight">
                 <IconButton>
