be762a380ef5fc92ed949e6b8a5ae04c30cec1e5
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-11 13:15:48 +0000

    fix: class to className

--- whatsapp-clone/src/ChatMessage.js
+++ whatsapp-clone/src/ChatMessage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./ChatMessage.css"
 
-function ChatMessage() {
+function ChatMessage(props) {
     return (
         <div className="chat__wrapper">
             <div className={`chat__bubble ${true && 'chat__me'}`}>
-                <div class="chat__txt">
-                    <p className="chat__name">Nkrumah Sarpong</p>
-                    <p className="chat__message">Hey Guys</p>
-                    <span className="chat__timestamp">10:20 pm</span>
+                <div className="chat__txt">
+                    <p className="chat__name">{props.name}</p>
+                    <p className="chat__message">{props.message}</p>
+                    <span className="chat__timestamp">{props.timestamp}</span>
                 </div>
             </div>
             <div className="chat__arrow"></div>
