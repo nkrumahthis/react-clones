3efc8091e4160ecab0497dca758d6d8419ea4f60
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-06-11 12:00:46 +0000

    feat: usestatevalue in App.js

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -5,9 +5,12 @@ import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './Login';
 
+import { useStateValue } from './StateProvider';
+
 function App() {
 
-  const [ user, setUser ] = useState(null);
+  const [{ user }, dispatch ] = useStateValue();
+
 
   return (
     <div className="app">
